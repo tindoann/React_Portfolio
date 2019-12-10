db6706d429c3401167240fbfd0ed412e34e5dcb9
tindoann <tindoann@gamil.com>
2019-12-10 16:40:38 -0500

    updated public title

---
+++ build/precache-manifest.65c5e5842387687bfb6f2ce71c2cc337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0932363942b6fa19b66f86bc7913458",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "1fe59ea211e11fb1a406",
+    "url": "/React_Portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "22ca3d54f7533cef722c",
+    "url": "/React_Portfolio/static/js/2.26fa9988.chunk.js"
+  },
+  {
+    "revision": "88dd02bda13cdbcf74e3b472e0a270c8",
+    "url": "/React_Portfolio/static/js/2.26fa9988.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fe59ea211e11fb1a406",
+    "url": "/React_Portfolio/static/js/main.3dcaf734.chunk.js"
+  },
+  {
+    "revision": "51bfd1a92198ac2e0a41",
+    "url": "/React_Portfolio/static/js/runtime-main.d0aa4d6d.js"
+  }
+]);
